17af73395e13f50cbb4355e8ee9031988a570241
CristobalAvalos <cristobal.avalos01@alumnos.ucn.cl>
2025-09-10 19:59:01 -0300

    nada importante
    nada importante

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -27,7 +27,7 @@ export class AuthService {
                     }
                 })
             );
-            return respon   se.data;
+            return response.data;
         } catch (error) {
 
 
